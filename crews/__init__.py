2e4673a0ae0f59ded35adccc045aabdd6cc4219f
sgazagnes <sgazagnes@gmail.com>
2025-06-16 09:51:37 +0100

    Remove pycache and pyc files from version control

--- crews/__init__.py
+++
@@ -1,8 +0,0 @@
-# crews/__init__.py
-"""
-Crews package for the Job Search System
-"""
-
-from .company_research import run_company_research
-
-__all__ = ['run_company_research']
--- crews/company_research/__init__.py
+++
@@ -1,8 +0,0 @@
-# crews/company_research/__init__.py
-"""
-Company research crew
-"""
-
-from .workflow import run_company_research, CompanyResearchWorkflow
-
-__all__ = ['run_company_research', 'CompanyResearchWorkflow']
